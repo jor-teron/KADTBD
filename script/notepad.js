7cd356b7b81e25e0dd934742434159736db224dc
J T <jor.teron@gmail.com>
2025-06-18 16:40:12 +0530

    Update notepad.js

--- script/notepad.js
+++ script/notepad.js
@@ -1,11 +1,23 @@
 const textarea = document.querySelector('.notepad-textarea');
 const saveButton = document.querySelector('.notepad-save');
+const downloadButton = document.querySelector('.notepad-download');
 const clearButton = document.querySelector('.notepad-clear');
 
 textarea.value = localStorage.getItem('notepad-text') || '';
 
 saveButton.addEventListener('click', () => {
   localStorage.setItem('notepad-text', textarea.value);
+});
+
+downloadButton.addEventListener('click', () => {
+  const text = textarea.value;
+  const blob = new Blob([text], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'My_note.txt';
+  a.click();
+  URL.revokeObjectURL(url);
 });
 
 clearButton.addEventListener('click', () => {
